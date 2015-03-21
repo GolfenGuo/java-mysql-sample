39d53ca702c8ae481198fdd982370ec4843a8df6
golfen <golfen.guo@daocloud.io>
2015-03-21 19:34:56 +0800

    initial commit

---
+++ src/test/java/io/daocloud/TestApp.java
@@ -0,0 +1,30 @@
+package io.daocloud;
+
+import junit.framework.*;
+
+public class TestApp extends TestCase {
+  
+  protected void setUp() {
+    // put common setup code in here
+  }
+   
+  protected void tearDown() {
+    // put common cleanup code in here
+  }
+
+  public void testInsertAndGetData() throws Exception {
+    Integer result = null;
+    try {
+      Storage storage = new Storage();
+      storage.populate();
+      result = storage.getScore();
+      System.out.println(result);
+    } catch (Exception e) {
+      System.out.println("Error testing database");
+      System.out.println(e.getMessage());
+    } finally {
+      assertEquals(new Integer(1234), result);
+    }
+  } 
+}
+
