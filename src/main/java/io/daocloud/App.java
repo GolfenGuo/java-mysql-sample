2b21ec8db505c7d72ef2fd99122fcc6a1f1a29ee
golfen <golfen.guo@daocloud.io>
2015-03-28 18:02:55 +0800

    no message

--- src/main/java/io/daocloud/App.java
+++ src/main/java/io/daocloud/App.java
@@ -8,7 +8,7 @@ public class App {
 
    public static void main(String[] args) {
 
-      final Storage storage;
+      final Storage storage = null;
       try {
         storage = new Storage();
            storage.populate();
